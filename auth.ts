5059a3c68d5f6f1db334690105e2b1c4dadf80ca
Mohammed Aminu <aminuolawalekan@gmail.com>
2026-04-26 01:28:39 +0200

    Fix authentication

--- auth.ts
+++ auth.ts
@@ -1,15 +1,8 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
-const ADMIN_EMAIL = "aminuolawalekan@gmail.com"
-
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Google],
-  callbacks: {
-    signIn({ user }) {
-      return user.email === ADMIN_EMAIL
-    },
-  },
   pages: {
     signIn: "/admin",
     error: "/admin",
